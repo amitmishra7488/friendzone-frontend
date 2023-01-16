96fb0a595f263988657df68e1d47e3bc66ed0540
Amit Kumar <amitmishra74880@gmail.com>
2023-01-16 23:34:29 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
+import ParticlesBg from 'particles-bg'
 
 function App() {
+  let config = {
+    num: [4, 7],
+    rps: 0.1,
+    radius: [5, 40],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-40, 40],
+    // body: "./img/icon.png", // Whether to render pictures
+    // rotate: [0, 20],
+    alpha: [0.6, 0],
+    scale: [1, 0.1],
+    position: "center", // all or center or {x:1,y:1,width:100,height:100}
+    color: ["random", "#ff0000"],
+    cross: "dead", // cross or bround
+    random: 15,  // or null,
+    g: 5,    // gravity
+    // f: [2, -1], // force
+    onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Login/>
+      <ParticlesBg type="custom" config={config} bg={true} />
     </div>
   );
 }
