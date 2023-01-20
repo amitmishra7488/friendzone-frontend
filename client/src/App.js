3b52607ff8c95127db389847aa6da5d7e92b7974
Amit Kumar <amitmishra74880@gmail.com>
2023-01-20 23:28:15 +0530

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,52 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/Login';
 import ParticlesBg from 'particles-bg'
-import Form from './switch/Form';
+import Profile from './components/Profile/Profile';
 
 function App() {
-//   "color"
-// "ball"
-// "lines"
-// "thick"
-// "circle"
-// "cobweb"
-// "polygon"
-// "square"
-// "tadpole"
-// "fountain"
-// "random"
-// "custom"
-  let config = {
-    num: [4, 7],
-    rps: 0.1,
-    radius: [5, 10],
-    life: [1.5, 3],
-    v: [2, 3],
-    tha: [-40, 40],
-    // body: "./img/icon.png", // Whether to render pictures
-    // rotate: [0, 20],
-    alpha: [0.6, 0],
-    scale: [1, 0.1],
-    position: "all", // all or center or {x:1,y:1,width:100,height:100}
-    color: ["random", "#ff0000"],
-    cross: "dead", // cross or bround
-    random: 15,  // or null,
-    g: 5,    // gravity
-    // f: [2, -1], // force
-    onParticleUpdate: (ctx, particle) => {
-        ctx.beginPath();
-        ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
-        ctx.fillStyle = particle.color;
-        ctx.fill();
-        ctx.closePath();
-    }
-  };
+
+
   return (
     <div className="App">
-      {/* <Form/> */}
-      <Login/>
-      {/* <ParticlesBg type="lines" config={config} bg={true} /> */}
-      {/* <ParticlesBg color="#ff0000" num={200} type="circle" bg={true} /> */}
+      {/* <Login/> */}
+      <Profile/>
+      
     </div>
   );
 }
---
+++ client/src/components/Profile/ProfileDetails.jsx
@@ -0,0 +1,71 @@
+import { Button } from '@chakra-ui/button';
+import { Image } from '@chakra-ui/image';
+import { Box, HStack, Text } from '@chakra-ui/layout'
+import { Tooltip } from '@chakra-ui/tooltip';
+import { fontSize, width } from '@mui/system';
+import React, { useEffect, useState } from 'react'
+import { GrUserSettings } from 'react-icons/gr'
+import { Icon } from '@chakra-ui/react'
+
+
+export default function ProfileDetails() {
+    const [profile, setProfile] = useState({});
+    const profilePic = async () => {
+        try {
+            const res = await fetch('https://randomuser.me/api/')
+            const data = await res.json();
+            console.log(data.results[0]);
+            setProfile(data.results[0]);
+            // console.log(profile)
+        }
+        catch (error) {
+            console.lof(error);
+        }
+    }
+
+    useEffect(() => {
+        profilePic();
+    }, [])
+    return (
+        <Box className='profileMainDiv' >
+            <Box className='profileContainer' display="flex" w="60%" m="auto" mt={50}>
+                <Box  w='30%'>
+                    <Box><Image borderRadius='full' boxSize='75%' border="2px solid #ccc"
+    objectFit='cover' src={"https://bit.ly/dan-abramov" } alt='profile' /></Box>
+                </Box>
+                <Box  w='70%'>
+                    <Box display="flex" flexDirection="column" >
+
+                        {/* 1st row */}
+                        <HStack spacing='4%' ml='20px'mb="2%">
+                            <Text>coolamitmishra</Text>
+                            <Button size="sm">Edit Profile</Button>
+                            <Tooltip hasArrow label='user setting' >
+                                <Icon as={GrUserSettings} boxSize={25}   />
+                            </Tooltip>   
+                        </HStack>
+
+
+
+                        <HStack spacing='8%' ml='20px' mb="2%">
+                            <Text>26 posts</Text>
+                            <Text>1k followers</Text>
+                            <Text>26 following</Text>
+                        </HStack>
+
+                        <Box ml='20px'>
+                        <Text p={0} fontSize="20px">Akshay Kumar</Text>
+                        <Text p={0}>❤️❤️Jai Mata Di❤️❤️<br />
+                            🚩🚩 जय श्री राम 🚩🚩<br />
+                            🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+                        </Text>
+
+
+                        </Box>
+
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
