8d24d5c6a8f5dd717424f2a39a7fd4276c68c1b4
Amit Kumar <amitmishra74880@gmail.com>
2023-01-21 22:18:20 +0530

    checking pagination

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/layout'
 import React from 'react'
+import ProfileCard from './ProfileCard'
 import ProfileDetails from './ProfileDetails'
 
 export default function Profile() {
   return (
     <Box>
         <ProfileDetails/>
+        <ProfileCard/>
     </Box>
   )
 }
