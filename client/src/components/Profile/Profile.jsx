84d4218a0ac520805934821e7a56b41a8998d82c
Amit Kumar <amitmishra74880@gmail.com>
2023-02-22 22:48:19 +0530

    changes

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,13 +1,21 @@
-import { Box } from '@chakra-ui/layout'
+import { Box,Flex } from '@chakra-ui/layout'
 import React from 'react'
+import Sidebar from '../Navbar/Sidebar'
 import ProfileCard from './ProfileCard'
 import ProfileDetails from './ProfileDetails'
 
 export default function Profile() {
   return (
-    <Box>
-        <ProfileDetails/>
-        <ProfileCard/>
-    </Box>
+    <Flex>
+      <Box w="20%" >
+          <Sidebar/>
+      </Box>
+      <Box w="80%">
+          <ProfileDetails/>
+          <ProfileCard/>
+      </Box>
+        
+    </Flex>
+   
   )
 }
--- client/src/components/Profile/ProfileDetails.jsx
+++ client/src/components/Profile/ProfileDetails.jsx
@@ -13,101 +13,167 @@ import {
     ModalOverlay,
     ModalContent,
     ModalHeader,
-    ModalFooter,
-    ModalBody,
     ModalCloseButton,
-  } from '@chakra-ui/react'
+    ModalBody,
+    ModalFooter,
+    FormControl,
+    FormLabel,
+    Input,
+} from '@chakra-ui/react'
 
 
 export default function ProfileDetails() {
     const cookies = new Cookies();
     const [profile, setProfile] = useState({});
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    // const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const display = async()=>{
+    const display = async () => {
 
-        try{
-            const res = await axios.get('http://localhost:8080/user/profile',{
+        try {
+            const res = await axios.get('http://localhost:8080/user/profile', {
                 headers: {
                     authorization: cookies.get('token')
-                  }
+                }
             })
             console.log(cookies.get('token'));
             console.log(res.data);
             setProfile(res.data);
         }
-        catch(error){
+        catch (error) {
             console.log(error);
         }
     }
-    
 
-    
+    const [isOpen, setIsOpen] = useState(false);
+    const [bio, setBio] = useState("");
+    const [dpLink, setDpLink] = useState("");
+    const [userName,setUserName] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        
+        if(bio==="" && dpLink==="" && userName===""){
+            alert("please enter specific details");
+        }
+        else{
+            editProfile(bio,dpLink,userName);
+        }
+        setIsOpen(false);
+        // console.log(profile);
+    }
+
+    const editProfile = async (bio,dpLink,userName) => {
+
+        try {
+            const res = await axios.put('http://localhost:8080/user/bioDP',
+            {
+                bio:bio,
+                dp:dpLink,
+                userName:userName
+            }, {
+                headers: {
+                    authorization: cookies.get('token')
+                }
+            })
+            console.log(cookies.get('token'));
+            console.log(res.data);
+            setProfile(res.data);
+            display();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
 
     useEffect(() => {
         display();
-        
+
     }, [])
     return (
         <Box className='profileMainDiv' >
-            {Object.keys(profile).length<=0?<Spinner textAlign="center" size='xl' />
-            :<Box className='profileContainer' display="flex" w="60%" m="auto" mt={50}>
-                <Box  w='30%'>
-                    <Box><Image borderRadius='full' boxSize='75%' border="2px solid #ccc" objectFit='cover' src={ profile.user.dp} alt='profile' /></Box>
-                </Box>
-                <Box  w='70%'>
-                    <Box display="flex" flexDirection="column" >
+            {Object.keys(profile).length <= 0 ? <Spinner textAlign="center" size='xl' />
+                : <Box className='profileContainer' display="flex" w="60%" m="auto" mt={50}>
+                    <Box w='30%'>
+                        <Box><Image borderRadius='full' boxSize='75%' border="2px solid #ccc" objectFit='cover' src={profile.user.dp} alt='profile' /></Box>
+                    </Box>
+                    <Box w='70%'>
+                        <Box display="flex" flexDirection="column" >
 
-                        {/* 1st row */}
-                        <HStack spacing='4%' ml='20px'mb="2%">
-                            <Text>{profile.user.name}</Text>
-                            <Button size="sm" onClick={onOpen}>Edit Profile</Button>
-                            <Tooltip hasArrow label='user setting' >
-                                <Box><Icon as={GrUserSettings} boxSize={25}   /></Box>
-                            </Tooltip>   
-                        </HStack>
+                            {/* 1st row */}
+                            <HStack spacing='4%' ml='20px' mb="2%">
+                                <Text>{profile.user.userName}</Text>
+                                <Button size="sm" onClick={() => setIsOpen(true)}>Edit Profile</Button>
+                                <Tooltip hasArrow label='user setting' >
+                                    <Box><Icon as={GrUserSettings} boxSize={25} /></Box>
+                                </Tooltip>
+                            </HStack>
 
 
 
-                        <HStack spacing='8%' ml='20px' mb="2%">
-                            <Text>26 posts</Text>
-                            <Text>{profile.user.followers.length} followers</Text>
-                            <Text>{profile.user.following.length} following</Text>
-                        </HStack>
+                            <HStack spacing='8%' ml='20px' mb="2%">
+                                <Text>26 posts</Text>
+                                <Text>{profile.user.followers>0?profile.user.followers.length :null} followers</Text>
+                                <Text>{profile.user.followers>0? profile.user.following.length:null} following</Text>
+                            </HStack>
 
-                        <Box ml='20px'>
-                        <Text p={0} fontSize="20px">{profile.user.name}</Text>
-                        <Text p={0}>{profile.user.bio}</Text>
+                            <Box ml='20px'>
+                                <Text p={0} fontSize="20px">{profile.user.name}</Text>
+                                <Text p={0}>{profile.user.bio}</Text>
+
+                            </Box>
 
                         </Box>
-
                     </Box>
-                </Box>
 
-            </Box>}
+                </Box>}
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-          <Text >
-  "The quick brown fox jumps over the lazy dog" is an English-language pangram—a
-  sentence that contains all of the letters of the English alphabet. Owing to
-  its existence, Chakra was created.
-</Text>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Edit Profile</ModalHeader>
+                    <ModalCloseButton />
+                    <form onSubmit={handleSubmit}>
+                        <ModalBody>
+                        <FormControl id="userName">
+                                <FormLabel>Add your userName</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={userName}
+                                    onChange={(event) => setUserName(event.target.value)}
+                                />
+                            </FormControl>
+                            <FormControl id="bio">
+                                <FormLabel>Add Your Bio</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={bio}
+                                    onChange={(event) => setBio(event.target.value)}
+                                />
+                            </FormControl>
+                            <FormControl id="dpLink">
+                                <FormLabel>Profile DP Link</FormLabel>
+                                <Input
+                                    type="text"
+                                    value={dpLink}
+                                    onChange={(event) => setDpLink(event.target.value)}
+                                />
+                            </FormControl>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button variant="ghost" mr={3} onClick={() => setIsOpen(false)}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme="blue" type="submit">
+                                Save
+                            </Button>
+                        </ModalFooter>
+                    </form>
+                </ModalContent>
+            </Modal>
         </Box>
     )
 }
